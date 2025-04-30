c1e2373e6d46147194697c4fd606c21d14af8e90
shanny2515 <damarshania05@gmail.com>
2025-04-30 23:33:49 +0300

    fix image filename typo and ensure proper slide functionality

--- javascript/about.js
+++ javascript/about.js
@@ -1,2 +1,8 @@
-const images = ["./images/chefs.jpeg","./images/service.jpeg","./images/decor.jpeg","./images/photographer.jpeg","./images/at work.jpeg"]
+const images = ["./images/chefs.jpeg","./images/service.jpeg","./images/decor.jpeg","./images/photography.jpeg","./images/at work.jpeg"]
 let currentIndex = 0;
+function moveSlide(step) {
+    currentIndex += step;
+    if (currentIndex < 0) currentIndex = images.length -1;
+    if (currentIndex >= images.length) currentIndex = 0;
+    document.getElementById("slide").src = images[currentIndex];
+}
